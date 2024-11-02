795375eb4ec52c5fba667a8ab341d9e60df3bc0d
Dmitriy Shein <409888@edu.itmo.ru>
2024-11-02 03:43:05 +0300

    AVL move to package

---
+++ src/Test.java
@@ -0,0 +1,40 @@
+import treeCollections.*;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        MySet st = new AVLTree();
+        Scanner in = new Scanner(System.in);
+
+        while (in.hasNext()) {
+            String command = in.next();
+            switch (command) {
+                case "+": {
+                    int x = in.nextInt();
+                    st.add(x);
+                    break;
+                }
+                case "-": {
+                    int x = in.nextInt();
+                    st.remove(x);
+                    break;
+                }
+                case "f": {
+                    int x = in.nextInt();
+                    System.out.println(st.contains(x));
+                    break;
+                }
+                case "p":
+                    st.printTree();
+                    break;
+                case "q":
+                    System.out.println("exit");
+                    System.exit(0);
+                    break;
+            }
+        }
+
+    }
+}
