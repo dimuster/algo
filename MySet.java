8bf6a131087459ae1c748d42c78ba82930172cdf
Dmitriy Shein <409888@edu.itmo.ru>
2024-11-02 03:42:06 +0300

    remove "public"

--- MySet.java
+++
@@ -1,8 +0,0 @@
-public interface MySet {
-    public int size();
-    public boolean isEmpty();
-    public boolean contains(int x);
-    public void add(int x);
-    public void remove(int x);
-    public void printTree();
-}
---
+++ src/treeCollections/MySet.java
@@ -0,0 +1,10 @@
+package treeCollections;
+
+public interface MySet {
+    int size();
+    boolean isEmpty();
+    boolean contains(int x);
+    void add(int x);
+    void remove(int x);
+    void printTree();
+}
